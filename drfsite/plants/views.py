caf349d7b4c3d9ee85a8a8b314ff568bd5f8274f
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-08-05 20:45:34 +0300

    dj rest #3

--- drfsite/plants/views.py
+++ drfsite/plants/views.py
@@ -1,12 +1,30 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Plants
 from .serializers import PlantsSerializer
 
 
-class PlantsAPIView(generics.ListAPIView):
-    queryset = Plants.objects.all()
-    serializer_class = PlantsSerializer
+# class PlantsAPIView(generics.ListAPIView):
+#     queryset = Plants.objects.all()
+#     serializer_class = PlantsSerializer
+
+
+class PlantsAPIView(APIView):
+    def get(self, request):
+        lst = Plants.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Plants.objects.create(
+            title=request.data['title'],
+            description=request.data['description'],
+            category_id=request.data['category_id']
+        )
+
+        return Response({'post': model_to_dict(post_new)})
 
 
